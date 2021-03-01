43d845a18b94e211a8ab7bf02aafe074635e10f7
NielsTRS <trsniels@gmail.com>
2021-03-01 21:19:52 +0100

    Edit form

--- src/View/User/connexion.php
+++ src/View/User/connexion.php
@@ -9,16 +9,11 @@
         <form class="col-md-10 mt-20px" action="<?php echo WEB_ROOT; ?>connexion" method="post">
             <div class="mb-3">
                 <label for="username" class="form-label">Nom d'utilisateur</label>
-                <input type="text" class="form-control" name="nom" aria-describedby="userHelp" required/>
+                <input type="text" class="form-control" id="username" name="nom" aria-describedby="userHelp" required/>
             </div>
             <div class="mb-3">
                 <label for="password" class="form-label">Mot de passe</label>
-                <div class="input-group">
-                    <input type="password" class="form-control" name="password" id="password" required/>
-                    <div class="input-group-addon">
-                        <a href="" id="togglePassword"><i class="fas fa-eye-slash" id="eyeIcon" aria-hidden="true"></i></a>
-                    </div>
-                </div>
+                <input type="password" class="form-control" name="password" id="password" required/>
                 <div id="userHelp" class="form-text">Ne partagez jamais votre mot de passe.</div>
             </div>
             <button type="submit" class="btn btn-primary" id="connect" name="connexion">Se connecter</button>
--- src/View/User/inscription.php
+++ src/View/User/inscription.php
@@ -9,36 +9,33 @@
         <form class="col-md-10 mt-20px" action="<?php echo WEB_ROOT; ?>inscription" method="post">
             <div class="mb-3">
                 <label for="name" class="form-label">Nom</label>
-                <input type="text" class="form-control" name="nom" placeholder="Dupont" required/>
+                <input type="text" id="name" class="form-control" name="nom" placeholder="Dupont" required/>
             </div>
             <div class="mb-3">
                 <label for="prenom" class="form-label">Prénom</label>
-                <input type="text" class="form-control" name="prenom" placeholder="Dupont" required/>
+                <input type="text" id="prenom" class="form-control" name="prenom" placeholder="Dupont" required/>
             </div>
             <div class="mb-3">
                 <label for="email" class="form-label">Adresse e-mail</label>
-                <input type="email" class="form-control" name="email" placeholder="test@example.com" required/>
+                <input type="email" id="email" class="form-control" name="email" placeholder="test@example.com"
+                       required/>
             </div>
             <div class="mb-3">
                 <label for="adresse" class="form-label">Adresse</label>
-                <input type="text" class="form-control" name="adresse" placeholder="15 rue des Champs" required/>
+                <input type="text" id="email" class="form-control" name="adresse" placeholder="15 rue des Champs"
+                       required/>
             </div>
             <div class="mb-3">
                 <label for="cp" class="form-label">Code Postal</label>
-                <input type="text" class="form-control" name="cp" required/>
+                <input type="text" id="cp" class="form-control" name="cp" required/>
             </div>
             <div class="mb-3">
                 <label for="ville" class="form-label">Ville</label>
-                <input type="text" class="form-control" name="ville" placeholder="Paris" required/>
+                <input type="text" id="ville" class="form-control" name="ville" placeholder="Paris" required/>
             </div>
             <div class="mb-3">
                 <label for="password" class="form-label">Mot de passe</label>
-                <div class="input-group">
-                    <input type="password" class="form-control" name="password" id="password" required/>
-                    <div class="input-group-addon">
-                        <a href="" id="togglePassword"><i class="fas fa-eye-slash" id="eyeIcon" aria-hidden="true"></i></a>
-                    </div>
-                </div>
+                <input type="password" class="form-control" name="password" id="password" required/>
                 <div id="userHelp" class="form-text">Ne partagez jamais votre mot de passe.</div>
             </div>
             <button type="submit" class="btn btn-primary" id="connect" name="inscription">S'inscrire</button>
